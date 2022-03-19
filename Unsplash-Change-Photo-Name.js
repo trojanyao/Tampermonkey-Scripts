a813b04a6304d1a5de8bc9ad787213777789147f
Trojan Yao <ytj1996@sina.com>
2022-03-19 20:11:21 +0800

    feat: 新增「Unsplash 下载文件名修改」脚本
    Unsplash 下载的图片默认文件名不包含照片信息，该脚本将照片信息写入文件名，并统一格式。

---
+++ Unsplash-Change-Photo-Name.js
@@ -0,0 +1,47 @@
+// ==UserScript==
+// @name         Unsplash-Change-Photo-Name
+// @namespace    http://tampermonkey.net/
+// @version      1.0
+// @description  Change the file name of download photo.
+// @author       TROJAN
+// @match        https://unsplash.com/photos/*
+// @grant        none
+// ==/UserScript==
+
+(function () {
+    'use strict';
+
+    async function changeName() {
+        // 获取下载按钮
+        let a = document.querySelector('header > div:last-child > div:last-child > div > a')
+
+        // ----- 图片信息 -----
+        // 发布日期
+        let dateStr = document.querySelector('time').innerText
+        let date = new Date(dateStr).toLocaleDateString().replace(/\//g, '.')
+        // 位置
+        let location = document.querySelector('header+div > div > div > div:last-child > div:nth-child(4) > div')
+        location = location?.innerText || undefined
+        // 作者
+        let photographer = document.querySelector('header+div > div > div > div > header > div > span > div:last-child > span a').href
+        photographer = photographer.match(/\@\w+/)[0]
+        // 链接
+        let link = document.URL.replace('https://', '')
+        // console.log('链接', link)
+        // 图片地址（跨域获取）
+        a.removeAttribute('target')
+        let imgUrl = a.href
+        const res = await fetch(imgUrl)
+        const blob = await res.blob()
+        imgUrl = window.URL.createObjectURL(blob)
+        // console.log('图片实际地址', imgUrl)
+        a.href = imgUrl
+
+        // 修改文件名
+        a.download = `${date} ${location ? '【' + location + '】' : ' '}Photo by ${photographer} [${link}].jpg`
+    }
+
+    window.onload = () => {
+        changeName()
+    }
+})();
