ae2d05a1fce9977fd270d71c43cbeaa1a32cd9a1
TROJAN <ytj1996@gmail.com>
2025-08-13 15:23:52 +0800

    fix(Unsplash): handle Unsplash website updates

--- Unsplash-Change-Photo-Name_user.js
+++ Unsplash-Change-Photo-Name_user.js
@@ -1,11 +1,11 @@
 ﻿// ==UserScript==
-// @license      MIT License
 // @name         Unsplash 下载文件名修改 Unsplash Change Photo Name
+// @license      MIT License
 // @namespace    http://tampermonkey.net/
 // @version      1.0
 // @description  Change the file name of download photo.
 // @author       TROJAN
-// @match        https://unsplash.com/photos/*
+// @match        https://unsplash.com/*
 // @require      https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.29.2/moment.min.js
 // @grant        none
 // ==/UserScript==
@@ -19,17 +19,8 @@
 
         // ----- 图片信息 -----
         // 发布日期
-        let date, dateStr = document.querySelector('time').innerText
-        if (dateStr.includes('ago')) {
-            // 最近发布，无确切日期
-            let daysAgo = dateStr.match(/\d+/g)?.[0]
-            console.log('几天前', daysAgo)
-            date = moment().subtract(daysAgo).format('YYYY.MM.DD')
-        } else {
-            // 较早发布，有准确日期
-            date = new Date(dateStr).toLocaleDateString('zh-CN', { year: 'numeric', month: '2-digit', day: '2-digit' }).replace(/\//g, '.')
-        }
-        console.log('日期', date)
+        const date = document.querySelector('time').getAttribute('datetime').slice(0, 10).replace(/-/g, ".");
+        // console.log('日期', date)
 
         // 位置
         let location = document.querySelector('header+div > div > div > div:last-child > div:nth-child(3) > div')
@@ -38,23 +29,29 @@
             location = document.querySelector('header+div > div > div > div:last-child > div:nth-child(4) > div')
             location = location?.innerText || undefined
         }
-        // 作者
-        let photographer = document.querySelector('header+div > div > div > div > header > div > span > div:last-child > span a').href
-        photographer = photographer.match(/\@\w+/)[0]
+
+        // 摄影师
+        const photographer = document.querySelector('body > div > div > div > div > div > div:nth-child(2) > div > header > div > span > div > div > a')?.innerText || '未知摄影师'
+
         // 链接
-        let link = document.URL.replace('https://', '')
+        let link = document.URL.replace('https://unsplash.com/photos/', '')
         // console.log('链接', link)
+
         // 图片地址（跨域获取）
-        a.removeAttribute('target')
-        let imgUrl = a.href
-        const res = await fetch(imgUrl)
-        const blob = await res.blob()
-        imgUrl = window.URL.createObjectURL(blob)
-        // console.log('图片实际地址', imgUrl)
-        a.href = imgUrl
+        try {
+            a.removeAttribute('target')
+            let imgUrl = a.href
+            const res = await fetch(imgUrl)
+            const blob = await res.blob()
+            imgUrl = window.URL.createObjectURL(blob)
+            console.log('图片实际地址', imgUrl)
+            a.href = imgUrl
+        } catch(error) {
+            console.error('[跨域获取图片 Blob 地址失败] ', error)
+        }
 
         // 修改文件名
-        a.download = `${date}${location ? '【' + location + '】' : ' '}Photo by ${photographer} [${link}].jpg`
+        a.download = `${date}${location ? '【' + location + '】' : ''} @${photographer} [${link}].jpg`
     }
 
     window.onload = () => {
