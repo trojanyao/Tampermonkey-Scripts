860ecfcf94ac1e80693ca6c153c6e6cbfb6f0d71
TROJAN <ytj1996@gmail.com>
2025-08-13 15:52:14 +0800

    fix(Unsplash): fix photographer getting error

--- Unsplash/Unsplash-Change-Photo-Name_user.js
+++ Unsplash/Unsplash-Change-Photo-Name_user.js
@@ -8,6 +8,8 @@
 // @match        https://unsplash.com/*
 // @require      https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.29.2/moment.min.js
 // @grant        none
+// @downloadURL https://update.greasyfork.org/scripts/441752/Unsplash%20%E4%B8%8B%E8%BD%BD%E6%96%87%E4%BB%B6%E5%90%8D%E4%BF%AE%E6%94%B9%20Unsplash%20Change%20Photo%20Name.user.js
+// @updateURL https://update.greasyfork.org/scripts/441752/Unsplash%20%E4%B8%8B%E8%BD%BD%E6%96%87%E4%BB%B6%E5%90%8D%E4%BF%AE%E6%94%B9%20Unsplash%20Change%20Photo%20Name.meta.js
 // ==/UserScript==
 
 (function () {
@@ -31,7 +33,8 @@
         }
 
         // 摄影师
-        const photographer = document.querySelector('body > div > div > div > div > div > div:nth-child(2) > div > header > div > span > div > div > a')?.innerText || '未知摄影师'
+        const photographer = document.querySelector('body > div > div > div > div > div > div:nth-child(2) > div > header > div > span > div > a')?.href?.replace('https://unsplash.com/', '') || '未知摄影师'
+        // console.log('摄影师', photographer)
 
         // 链接
         let link = document.URL.replace('https://unsplash.com/photos/', '')
@@ -51,7 +54,8 @@
         }
 
         // 修改文件名
-        a.download = `${date}${location ? '【' + location + '】' : ''} @${photographer} [${link}].jpg`
+        a.download = `${date}${location ? '【' + location + '】' : ''} ${photographer} [${link}].jpg`
+        console.log('✅ 可以下载：', a.download)
     }
 
     window.onload = () => {
